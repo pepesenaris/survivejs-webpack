1a68408ca5626d6bcc98194b76a6c327b84a11c7
Jose J Señaris <pepe.senaris@alayacare.com>
2018-12-04 17:16:24 -0500

    Add postcss with autoprefixer

--- webpack.parts.js
+++ webpack.parts.js
@@ -4,35 +4,34 @@
  * Taken from https://survivejs.com/webpack/developing/composing-configuration/
  */
 
+exports.devServer = ({ host, port } = { host: '0.0.0.0', port: '1337' }) => ({
+  devServer: {
+    stats: 'errors-only',
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    open: true,
+    overlay: true,
+  },
+});
 
-exports.devServer = ({ host, port  } = {host: "0.0.0.0", port: "1337"}) => ({
-    devServer: {
-      stats: "errors-only",
-      host, // Defaults to `localhost`
-      port, // Defaults to 8080
-      open: true,
-      overlay: true,
-    },
-  });
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
 
-  exports.loadCSS = ({ include, exclude } = {}) => ({
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          include,
-          exclude,
-  
-          use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.less$/,
-            use: ["style-loader", "css-loader", "less-loader"],
-          },
-          {
-            test: /\.scss$/,
-            use: ["style-loader", "css-loader", "sass-loader"],
-          },
-      ],
-    },
-  });
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
