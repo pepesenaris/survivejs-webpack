516d6d69c64983f63f8418971e94e1225f7cba4e
Jose J Señaris <pepe.senaris@alayacare.com>
2018-12-04 17:07:22 -0500

    Install less and sass loaders

--- webpack.parts.js
+++ webpack.parts.js
@@ -25,6 +25,14 @@ exports.devServer = ({ host, port  } = {host: "0.0.0.0", port: "1337"}) => ({
   
           use: ["style-loader", "css-loader"],
         },
+        {
+            test: /\.less$/,
+            use: ["style-loader", "css-loader", "less-loader"],
+          },
+          {
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"],
+          },
       ],
     },
   });
