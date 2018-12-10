81450bedf772cc83d7700af164e041d3efe4c755
Jose J Señaris <pepe.senaris@alayacare.com>
2018-12-10 11:46:37 -0500

    Install @babel/plugin-syntax-dynamic-import

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,21 @@
 module.exports = function(api) {
-  api.cache(true)
+  api.cache(true);
   const presets = ['@babel/preset-env'];
-  // const plugins = [
-  //   '@babel/plugin-transform-runtime',
-  //   ['@babel/plugin-proposal-decorators', { legacy: true }],
-  //   '@babel/plugin-proposal-function-sent',
-  //   '@babel/plugin-proposal-export-default-from',
-  //   '@babel/plugin-proposal-export-namespace-from',
-  //   '@babel/plugin-proposal-numeric-separator',
-  //   '@babel/plugin-proposal-throw-expressions',
-  //   '@babel/plugin-proposal-class-properties',
-  //   '@babel/plugin-proposal-json-strings',
-  // ];
+  const plugins = [
+    '@babel/plugin-syntax-dynamic-import',
+    //   '@babel/plugin-transform-runtime',
+    //   ['@babel/plugin-proposal-decorators', { legacy: true }],
+    //   '@babel/plugin-proposal-function-sent',
+    //   '@babel/plugin-proposal-export-default-from',
+    //   '@babel/plugin-proposal-export-namespace-from',
+    //   '@babel/plugin-proposal-numeric-separator',
+    //   '@babel/plugin-proposal-throw-expressions',
+    //   '@babel/plugin-proposal-class-properties',
+    //   '@babel/plugin-proposal-json-strings',
+  ];
 
   return {
     presets,
-    // plugins,
+    plugins,
   };
 };
